198c6deaa1c26ba29e167e70062f86fa02d95e9d
SunnyStardust <nx4@rice.edu>
2024-01-06 23:59:37 -0600

    update

--- gcn/run_protein.sh
+++ gcn/run_protein.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-source ~/miniconda3/bin/activate deeplearning
 
-for units in 10000 15000 20000; do
-    for lr in 0.1 0.01 0.001 0.0001 0.00001; do
+for units in 25000 50000; do
+    for lr in 0.001 0.0001 0.00001; do
         for layer in 1 2 3; do
             echo "$units $lr $layer"
             python train_gist_noniid.py --dataset=PROTEINS --n-hidden="$units" --lr="$lr" --n-layers="$layer" --num_subnet=10
